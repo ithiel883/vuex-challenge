2212df2c3fc1a74eb3ed902d207de6045da6be97
ithiel883 <ajuka.zephiniah@gmail.com>
2022-02-11 23:41:34 +0100

    more functionalites for authentication and header

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,36 @@
 import { createStore } from "vuex";
 import productsModule from './modules/products.js'
-import cartModule from './modules/cart,js'
+import cartModule from './modules/carts.js'
 const store = createStore({
     modules: {
         prods: productsModule,
         cart: cartModule
+    },
+    state() {
+        return  {
+            isLoggedIn: false
+        }
+    },
+    mutations: {
+        login(state) {
+            state.isLoggedIn = true;
+          },
+          logout(state) {
+            state.isLoggedIn = false;
+          },
+    },
+    actions: {
+        login(context) {
+            context.commit('login')
+        },
+        logout(context) {
+            context.commit(logout)
+        }
+    },
+    getters: {
+        isAuthenticated(state) {
+            return state.isLoggedIn
+        }
     }
 })
 
