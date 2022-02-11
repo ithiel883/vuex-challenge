43ecbca60d247b7a3fe2739a8d138196860e5990
ithiel883 <ajuka.zephiniah@gmail.com>
2022-02-11 22:27:41 +0100

    added product and cart modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore } from "vuex";
 import productsModule from './modules/products.js'
-
+import cartModule from './modules/cart,js'
 const store = createStore({
     modules: {
-        prods: productsModule
+        prods: productsModule,
+        cart: cartModule
     }
 })
 
